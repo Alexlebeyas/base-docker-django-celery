41ee69a0d1e870936c580735fec25d51a315f3c3
Gabriel <Gabriel>
2016-11-18 18:43:47 -0500

    Ajustement page 404

--- project_starter/apps/front/views.py
+++ project_starter/apps/front/views.py
@@ -7,9 +7,6 @@ def home(request):
     return TemplateResponse(request, 'home.html', {
     })
 
-def sample(request):
-    return TemplateResponse(request, 'sample.html', {
-    })
 
 def error(request):
     return TemplateResponse(request, 'custom-404.html', {
