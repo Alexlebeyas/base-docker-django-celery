08328302ad8c799db257ded38e56157b31ca942b
Guy <guy@nixa.ca>
2016-12-30 15:47:07 -0500

    Email: default context

--- project_starter/libs/emails/urls.py
+++ project_starter/libs/emails/urls.py
@@ -5,6 +5,7 @@ __author__ = 'snake'
 if settings.DEBUG:
     from django.conf.urls import url
     from . import views
+
     urlpatterns = [
         url('^$', views.template_list, name='email_template_list'),
         url('^(?P<pk>.+?)/$', views.template_single, name='email_template_single'),
