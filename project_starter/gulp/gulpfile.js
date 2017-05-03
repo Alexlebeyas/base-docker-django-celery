5d4c05a3cf785e44296746c96f1cb279be52d0c4
Julien <julien@nixa.ca>
2017-05-03 17:15:07 -0400

    new gulpfile

--- project_starter/gulp/gulpfile.js
+++ project_starter/gulp/gulpfile.js
@@ -1,92 +1,178 @@
-var gulp = require('gulp'),
-  plumber = require('gulp-plumber'),
-  rename = require('gulp-rename'),
-  autoprefixer = require('gulp-autoprefixer'),
-  minifycss = require('gulp-clean-css'),
-  sass = require('gulp-sass'),
-  browserSync = require('browser-sync'),
-  concat = require('gulp-concat'),
-  reload = browserSync.reload,
-  paths = {
-    src: 'scss/**/*.scss',
-    main: 'scss/main.scss',
-    admin: 'scss/admin.scss',
-    jsvendors: ['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'],
-    cssvendors: ['vendors/bootstrap.scss','node_modules/font-awesome/scss/font-awesome.scss'],
-    fontsvendors: ['node_modules/font-awesome/fonts/fontawesome-webfont.ttf', 'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
-              'node_modules/font-awesome/fonts/fontawesome-webfont.woff2', 'node_modules/font-awesome/fonts/fontawesome-webfont.eot', 'node_modules/font-awesome/fonts/fontawesome-webfont.svg']
-  },
-  dest = {
-    css: '../apps/front/static/css/',
-    admin: '../apps/custom_admin/static/custom_admin/css/',
-    scripts: '../apps/front/static/js/',
-    fonts: '../apps/front/static/fonts/'
-  };
+"use strict";
 
-gulp.task('styles', function(){
-  gulp.src([paths.main], { sourcemap: true })
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
-    //.pipe(minifycss())
-    .pipe(gulp.dest(dest.css))
-    .pipe(reload({ stream:true }));
+const gulp = require("gulp");
+const util = require("gulp-util");
+const babelify = require("babelify");
+const es2015 = require("babel-preset-es2015");
+const browserify = require("browserify");
+const streamify = require("gulp-streamify");
+const source = require("vinyl-source-stream");
+const sourcemaps = require("gulp-sourcemaps");
+const watchify = require("watchify");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const minifycss = require("gulp-clean-css");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const es = require("event-stream");
+const tap = require("gulp-tap");
+const runSequence = require('run-sequence');
+const path = require("path");
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+/**
+ *======================
+ *     Paths
+ * ======================
+ */
+
+const paths = {
+    styles: {
+        src: "scss/**/*.scss",
+        main: "scss/main.scss",
+        admin: "scss/admin.scss",
+        vendors: ["vendors/bootstrap.scss", "node_modules/font-awesome/scss/font-awesome.scss"],
+        dist: {
+            css: "../apps/front/static/css/",
+            admin: "../apps/custom_admin/static/custom_admin/css/"
+        }
+    },
+    scripts: {
+        src: "../apps/**/src/app*.js",
+        dist: "static"
+    },
+    fonts: {
+        vendors: ["node_modules/font-awesome/fonts/fontawesome-webfont.ttf", "node_modules/font-awesome/fonts/fontawesome-webfont.woff",
+            "node_modules/font-awesome/fonts/fontawesome-webfont.woff2", "node_modules/font-awesome/fonts/fontawesome-webfont.eot", "node_modules/font-awesome/fonts/fontawesome-webfont.svg"],
+        dist: "../apps/front/static/fonts/"
+    }
+};
+
+const webServer = "web";
+
+/**
+ *=======================
+ *      JS
+ * ======================
+ */
+
+var files = [];
+
+gulp.task("load-files", function () {
+    return gulp.src(paths.scripts.src)
+        .pipe(tap(function (file) {
+            files.push(file.path);
+        }));
 });
 
-gulp.task('cssadmin', function(){
-  gulp.src(paths.admin, { sourcemap: true })
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(concat('admin.css'))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(minifycss())
-    .pipe(gulp.dest(dest.admin));
+gulp.task("browserify", function () {
+    var tasks = files.map(function (entry) {
+
+        var bundler = watchify(
+            browserify({
+                entries: [entry],
+                debug: true,
+                cache: {}, packageCache: {}, fullPaths: true
+            }).transform(babelify, {presets: [es2015]})
+        );
+        var watch = watchBundle(bundler, entry);
+        bundler.on("update", watch);
+        bundler.on("log", util.log);
+        return watch();
+    });
+    return es.merge(tasks);
 });
 
-gulp.task('cssvendors', function(){
-  gulp.src(paths.cssvendors, { sourcemap: true })
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(concat('vendors.css'))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(minifycss())
-    .pipe(gulp.dest(dest.css));
+function watchBundle(bundler, entry) {
+    return function () {
+        return bundler.bundle()
+            .on('error',function (err) {
+                    console.log(err);
+                    this.emit('end');
+                })
+            .pipe(source("app.js"))
+            .pipe(buffer())
+            .pipe(sourcemaps.init())
+            .pipe(streamify(uglify()))
+            .pipe(rename({extname: ".min.js"}))
+            .pipe(sourcemaps.write("./"))
+            .pipe(gulp.dest(function () {
+                return entry.slice(0, entry.indexOf(paths.scripts.dist) + paths.scripts.dist.length) + "/js";
+            }));
+    };
+}
+
+/**
+ *=======================
+ *      SASS / FONTS
+ * ======================
+ */
+
+gulp.task("styles", function () {
+    gulp.src([paths.styles.main])
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: "compact", sourceComments: "map"}))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(paths.styles.dist.css))
+        .pipe(reload({stream: true}));
 });
 
-gulp.task('jsvendors', function(){
-  gulp.src(paths.jsvendors)
-    .pipe(concat('vendors.js'))
-    .pipe(gulp.dest(dest.scripts));
+gulp.task("cssadmin", function () {
+    gulp.src(paths.styles.admin, {sourcemap: true})
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat("admin.css"))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(paths.styles.dist.admin));
 });
 
-gulp.task('fontsvendors', function(){
-  gulp.src(paths.fontsvendors)
-    .pipe(gulp.dest(dest.fonts));
+gulp.task("cssvendors", function () {
+    gulp.src(paths.styles.vendors)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat("vendors.css"))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(paths.styles.dist.css));
 });
+
+gulp.task("fontsvendors", function () {
+    gulp.src(paths.fonts.vendors)
+        .pipe(gulp.dest(paths.fonts.dist));
+});
+
+/**
+ *=======================
+ *      BUILD TASKS
+ * ======================
+ */
 
 gulp.task('browsersync', ['styles'], function(){
   browserSync({
-    proxy: "127.0.0.1:8000"
+    proxy: webServer+":8000"
   });
-  gulp.watch(paths.src, ['styles']);
+  gulp.watch(paths.styles.src, ['styles']);
 });
 
-gulp.task('vendors', ['cssvendors', 'jsvendors', 'fontsvendors']);
+gulp.task("watchify", function () {
+    runSequence(
+        "load-files",
+        "browserify"
+    )
+});
 
-gulp.task('watch', ['default']);
+gulp.task('vendors', ['cssvendors', 'fontsvendors']);
 
-gulp.task('default', ['styles', 'cssadmin'] ,function(){
-  gulp.watch(paths.src, ['styles', 'cssadmin']);
-}); 
+gulp.task("watch-sass",  ["styles", "cssadmin"], function () {
+    gulp.watch(paths.styles.src, ["styles", "cssadmin"]);
+});
+
+gulp.task("watch", ["watch-sass", "watchify"]);
+
+gulp.task("default", ['watch']);
