e40721a4a2d059190e782c7ce738d717c7534c3e
thrill <philippe@nixa.ca>
2017-06-19 15:20:33 -0400

    little correction on js tools

--- project_starter/apps/front/static/src/tools/cookie.es6.js
+++ project_starter/apps/front/static/src/tools/cookie.es6.js
@@ -2,7 +2,7 @@ class Cookie {
     constructor() {
         this._cookies = {};
         for (let cookie of document.cookie.split(';')) {
-            let [name, value] = cookie.split('=');
+            let [name, value] = cookie.trim().split('=');
             this._cookies[name] = decodeURIComponent(value);
         }
     }
