9a1f1c75ed1c07cadfcf47cc6136d71e21df3d83
thrill <philippe@nixa.ca>
2017-06-19 14:53:57 -0400

    little correction on js tools

--- project_starter/apps/front/static/src/tools/cookie.es6.js
+++ project_starter/apps/front/static/src/tools/cookie.es6.js
@@ -11,29 +11,3 @@ class Cookie {
 }
 
 export default new Cookie()
-
-function setContactFormListener() {
-   $('section#contact form:first').submit(function(e){
-       e.preventDefault();
-       var _self = $(this);
-       $.ajax({
-           method: "POST",
-           url: _self.attr('action'),
-           data: _self.serialize(),
-           success: function() {
-               // handle success
-               _self.hide();
-               $('section#contact div.success-message:first').show();
-           },
-           error: function(data) {
-               // handle errors
-               var errors = JSON.parse(data.responseJSON);
-               for (var key in errors) {
-                   for (var i = 0; i < errors[key].length; i++) {
-                       $('#error_' + key).html(errors[key][i]["message"]);
-                   }
-               }
-           }
-       })
-   })
-}
