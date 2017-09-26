3d5b680e3814a2ad45a52c25e12061af525569fb
ryan <ryan@nixa.ca>
2017-09-26 14:22:36 -0400

    correct configuration for profile

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -10,15 +10,4 @@ from nixa_emails.mixins import EmailMixin
 class Profile(EmailMixin):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if instance.profile:
-        instance.profile.save()
-    else:
-        Profile.objects.create(user=instance)
 
