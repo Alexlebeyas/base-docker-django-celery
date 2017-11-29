6351960cd9cb7fbed618d300a780f815e4ca5335
JP <jp@nixa.ca>
2017-11-29 11:42:19 -0500

    - formatting.

--- apps/front/sitemap.py
+++ apps/front/sitemap.py
@@ -1,7 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from django.core.urlresolvers import reverse
 
-__author__ = 'marcantoine'
 
 class FrontendSitemap(Sitemap):
     changefreq = 'monthly'
